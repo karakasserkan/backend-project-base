18b614ccfda1a3132eab4b5fa1d47906d191675e
karakasserkan <karakasserkangit@gmail.com>
2026-02-18 21:39:41 +0300

    Implement database connection and add models for user management

---
+++ api/db/Database.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+let instance = null;
+class Database {
+  constructor() {
+    if (!instance) {
+      this.mongoConnection = null;
+      instance = this;
+    }
+    return instance;
+  }
+
+  async connect(options) {
+    try {
+      console.log("Connecting to database...");
+      let db = await mongoose.connect(options.CONNECTION_STRING);
+      this.mongoConnection = db;
+      console.log("Database connection successful");
+    } catch (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  }
+}
+
+module.exports = Database;
