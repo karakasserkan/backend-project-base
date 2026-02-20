7d6471af68b30de55890c3c9e279a5732526cd05
karakasserkan <karakasserkangit@gmail.com>
2026-02-20 17:33:30 +0300

    Add CRUD operations for categories and implement custom error handling

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -1,9 +1,96 @@
 var express = require("express");
 var router = express.Router();
+const Categories = require("../db/models/Categories");
+const Response = require("../lib/Response");
+const CustomError = require("../lib/Error");
+const Enum = require("../config/Enum");
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.json({ success: true });
+/**
+ * CRUD
+ * - Create: POST /categories
+ * - Read: GET /categories
+ * - Update: PUT /categories/:id
+ * - Delete: DELETE /categories/:id
+ */
+
+/* GET categories listing. */
+router.get("/", async (req, res, next) => {
+  try {
+    // Select sorgusu yazılan kısım.
+
+    let categories = await Categories.find({});
+
+    res.json(Response.successResponse(categories));
+  } catch (err) {
+    let errorResponse = Response.errorResponse(err);
+    res.status(errorResponse.code).json(Response.errorResponse(err));
+  }
+});
+
+router.post("/add", async (req, res, next) => {
+  let body = req.body;
+  try {
+    if (!body.name)
+      throw new CustomError(
+        Enum.HTTP_CODES.BAD_REQUEST,
+        "Name is required",
+        "Name field is missing in the request body",
+      );
+
+    let category = new Categories({
+      name: body.name,
+      is_active: true,
+      created_by: req.user?.id,
+    });
+
+    await category.save();
+
+    res.json(Response.successResponse({ success: true }));
+  } catch (err) {
+    let errorResponse = Response.errorResponse(err);
+    res.status(errorResponse.code).json(errorResponse);
+  }
+});
+
+router.post("/update", async (req, res) => {
+  let body = req.body;
+  try {
+    if (!body._id)
+      throw new CustomError(
+        Enum.HTTP_CODES.BAD_REQUEST,
+        "Id is required",
+        "Id field is missing in the request body",
+      );
+
+    let update = {};
+
+    if (body.name) update.name = body.name;
+    if (typeof body.is_active === "boolean") update.is_active = body.is_active;
+
+    await Categories.updateOne({ _id: body._id }, update);
+
+    res.json(Response.successResponse({ success: true }));
+  } catch (err) {
+    let errorResponse = Response.errorResponse(err);
+    res.status(errorResponse.code).json(errorResponse);
+  }
+});
+
+router.delete("/delete", async (req, res) => {
+  let body = req.body;
+  try {
+    if (!body._id)
+      throw new CustomError(
+        Enum.HTTP_CODES.BAD_REQUEST,
+        "Id is required",
+        "Id field is missing in the request body",
+      );
+    await Categories.deleteOne({ _id: body._id });
+    res.json(Response.successResponse({ success: true }));
+  } catch (err) {
+    let errorResponse = Response.errorResponse(err);
+    res.status(errorResponse.code).json(errorResponse);
+  }
 });
 
 module.exports = router;
