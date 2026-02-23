6a492eaf97be683d5ef7f26cadf76680a95bba5c
karakasserkan <karakasserkangit@gmail.com>
2026-02-23 15:21:39 +0300

    Implement logging functionality using Winston, create LoggerClass for structured logging, and integrate logging in categories route

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -5,6 +5,7 @@ const Response = require("../lib/Response");
 const CustomError = require("../lib/Error");
 const Enum = require("../config/Enum");
 const AuditLogs = require("../lib/AuditLogs");
+const logger = require("../lib/logger/LoggerClass");
 
 /**
  * CRUD
@@ -48,9 +49,11 @@ router.post("/add", async (req, res, next) => {
     await category.save();
 
     AuditLogs.info(req.user?.email, "Categories", "Add", category);
+    logger.info(req.user?.email, "Categories", "Add", category);
 
     res.json(Response.successResponse({ success: true }));
   } catch (err) {
+    logger.error(req.user?.email, "Categories", "Add", err);
     let errorResponse = Response.errorResponse(err);
     res.status(errorResponse.code).json(errorResponse);
   }
